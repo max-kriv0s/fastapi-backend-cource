6c1c7faaa484d2a892a8597426d15936b62af957
Maxim Krivoshein <max.kriv0s@gmail.com>
2025-01-31 01:11:39 +0300

    feat: pytest setup and add fixture for tests

--- app/config.py
+++ app/config.py
@@ -1,12 +1,29 @@
+from typing import Literal
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    MODE: Literal['DEV', 'TEST', 'PROD']
+    
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
     DB_PASS: str
     DB_NAME: str
+    
+    @property
+    def database_url(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+        
+    TEST_DB_HOST: str
+    TEST_DB_PORT: int
+    TEST_DB_USER: str
+    TEST_DB_PASS: str
+    TEST_DB_NAME: str
+
+    @property
+    def test_database_url(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
     
     ACCESS_TOKEN_ALGORITHM: str
     ACCESS_TOKEN_SECRET: str
@@ -21,10 +38,6 @@ class Settings(BaseSettings):
     SMTP_PORT: int
     SMTP_USER: str
     SMTP_PASS: str
-    
-    @property
-    def database_url(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
     model_config = SettingsConfigDict(env_file=".env")
     
--- app/database.py
+++ app/database.py
@@ -4,8 +4,14 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from app.config import settings
 
-engine = create_async_engine(settings.database_url)
-engine_nullpool = create_async_engine(settings.database_url, poolclass=NullPool)
+database_url = settings.database_url
+database_params = {}
+if settings.MODE == 'TEST':
+    database_url = settings.test_database_url
+    database_params = {"poolclass": NullPool}
+
+engine = create_async_engine(database_url, **database_params)
+engine_nullpool = create_async_engine(database_url, poolclass=NullPool)
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 async_session_maker_nullpool = async_sessionmaker(engine_nullpool, expire_on_commit=False)
